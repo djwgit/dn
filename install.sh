2eaec7236a5e0a7c78a01de8939f57429e575bc1
Jianwei Dou <jdou@esri.com>
2019-11-06 15:11:32 -0800

    install notes

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "export DN_PATH=~/dn/notes" >> ~/.zshrc
 echo "[ -f ~/dn/dn ] && source ~/dn/dn" >> ~/.zshrc
 echo "installed to ~/.zshrc"
 
-echo "Note:"
+echo "Notes:"
 echo "====="
-echo "default notes folder set to ~/dn/notes, could set it to a folder under onedrive folder for auto-sync"
+echo " - default notes folder set to ~/dn/notes."
+echo " - in .zshrc/.bashrc, could set DN_PATH to a folder under onedrive folder for auto-sync."
