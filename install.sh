47e29d822553b14ab1a688e951cd73e5c210a322
Jianwei Dou <jdou@esri.com>
2019-11-06 15:00:04 -0800

    add .txt extension name for onedrive online edit/view

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 mkdir -p ./notes
 
+# could set DN_PATH to a folder under your onedrive folder, then it will be auto-sync'ed to onedrive
+echo "export DN_PATH=~/dn" >> ~/.bashrc
 echo "[ -f ~/dn/.dn ] && source ~/dn/.dn" >> ~/.bashrc
 echo "installed to ~/.bashrc"
 
+echo "export DN_PATH=~/dn" >> ~/.zshrc
 echo "[ -f ~/dn/.dn ] && source ~/dn/.dn" >> ~/.zshrc
 echo "installed to ~/.zshrc"
